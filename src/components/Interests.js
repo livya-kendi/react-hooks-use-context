72ed71938dda7a05d8c43fd4758b8a7fc649e194
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-07-25 17:43:38 +0300

    done

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React,{useContext} from "react";
+import { ThemeContext } from "../context/theme";
+import { UserContext } from "../context/user";
 
-function Interests({ interests, theme }) {
+function Interests() {
+
+  const { theme } = useContext(ThemeContext);
+  const {user} = useContext(UserContext)
   return (
     <div>
       <h4>Interests</h4>
       <ul className={theme}>
-        {interests.map((interest) => (
+        {user.interests.map((interest) => (
           <li key={interest}>{interest}</li>
         ))}
       </ul>
